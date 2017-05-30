19301d401d665bd8df537397221826f60eee9be0
Michael Reda <michaelreda96@gmail.com>
2017-05-30 19:05:17 +0200

    tokens

--- const.js
+++ const.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // Wit.ai parameters
-const WIT_TOKEN = process.env.WIT_TOKEN;
+// const WIT_TOKEN = process.env.WIT_TOKEN;
+const WIT_TOKEN = "LJBITS2IOGPLPDTVH5YYDSW2OTCD6YKZ";
 if (!WIT_TOKEN) {
   throw new Error('missing WIT_TOKEN');
 }
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+//const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = "EAAL0VIHjZAZAgBAIg0QRTxz3oEYXL6vr8crg5YCuIapca1AZAcghDCLg2QbX7epmw7WJtWSWqGZAk0KhdeTcHMBrM4cDZCZCLtzYIj04tZA4jp4XkCDWrxERZAZCmE1EZAjGOMF0ejS1zxaX4awgFZC4NZB5IaN0ZAfXTBIbF6MYQW9GWnQZDZD";
 
 var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 if (!FB_VERIFY_TOKEN) {
