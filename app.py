e45fb29b8d26162afaffb13c68a80ca98910a1bc
Michael Reda <michaelreda96@gmail.com>
2017-05-31 03:03:46 +0200

    consoling

--- app.py
+++ app.py
@@ -19,36 +19,36 @@ def verify():
     return "Hello world!", 200
 
 
-# @app.route('/', methods=['POST'])
-# def webhook():
-# 	data = request.get_json()
-# 	log(data)
-#
-# 	if data['object'] == 'page':
-# 		for entry in data['entry']:
-# 			for messaging_event in entry['messaging']:
-#
-# 				# IDs
-# 				sender_id = messaging_event['sender']['id']
-# 				recipient_id = messaging_event['recipient']['id']
-#
-# 				if messaging_event.get('message'):
-# 					# Extracting text message
-# 					if 'text' in messaging_event['message']:
-# 						messaging_text = messaging_event['message']['text']
-# 					else:
-# 						messaging_text = 'no text'
-#
-# 					# Echo
-# 					response = messaging_text
-# 					bot.send_text_message(sender_id, response)
-#
-# 	return "ok", 200
-#
-#
-# def log(message):
-# 	print(message)
-# 	sys.stdout.flush()
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	log(data)
+
+	# if data['object'] == 'page':
+	# 	for entry in data['entry']:
+	# 		for messaging_event in entry['messaging']:
+    #
+	# 			# IDs
+	# 			sender_id = messaging_event['sender']['id']
+	# 			recipient_id = messaging_event['recipient']['id']
+    #
+	# 			if messaging_event.get('message'):
+	# 				# Extracting text message
+	# 				if 'text' in messaging_event['message']:
+	# 					messaging_text = messaging_event['message']['text']
+	# 				else:
+	# 					messaging_text = 'no text'
+    #
+	# 				# Echo
+	# 				response = messaging_text
+	# 				bot.send_text_message(sender_id, response)
+    #
+	# return "ok", 200
+
+
+def log(message):
+	print(message)
+	sys.stdout.flush()
 
 # ON_HEROKU = os.environ.get('ON_HEROKU')
 #
