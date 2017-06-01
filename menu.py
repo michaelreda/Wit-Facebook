2d2dfdd8022ed790729120a210fd5eef9b161688
Michael Reda <michaelreda96@gmail.com>
2017-06-01 05:09:12 +0200

    fixing

--- menu.py
+++ menu.py
@@ -1,7 +1,8 @@
-from fbmq import Page
+from fbmq import Page,Template
+
 PAGE_ACCESS_TOKEN = "EAAL0VIHjZAZAgBAIg0QRTxz3oEYXL6vr8crg5YCuIapca1AZAcghDCLg2QbX7epmw7WJtWSWqGZAk0KhdeTcHMBrM4cDZCZCLtzYIj04tZA4jp4XkCDWrxERZAZCmE1EZAjGOMF0ejS1zxaX4awgFZC4NZB5IaN0ZAfXTBIbF6MYQW9GWnQZDZD"
 
-page = fbmq.Page(PAGE_ACCESS_TOKEN)
+page = Page(PAGE_ACCESS_TOKEN)
 
 page.show_persistent_menu([Template.ButtonPostBack('MENU1', 'MENU_PAYLOAD/1'),
 						   Template.ButtonPostBack('MENU2', 'MENU_PAYLOAD/2')])
